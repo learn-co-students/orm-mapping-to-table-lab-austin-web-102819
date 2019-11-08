6ae3fcb436551ec47c89dc4b95866f4b8ac41316
Yining Wang <yining.wang@utexas.edu>
2019-11-07 22:00:16 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,43 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_reader :id
+  attr_accessor :name, :grade
+
+  def initialize(name, grade, id: nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def save
+    sql = 
+    <<-SQL
+    insert into students (name, grade) values (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create_table
+    sql = 
+    <<-SQL
+    create table students (id integer primary key, name text, grade text)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = 
+    <<-SQL
+    drop table students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(hash)
+    new_student = Student.new(hash[:name], hash[:grade])
+    new_student.save
+    new_student
+  end
+
 end
